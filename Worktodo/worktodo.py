f43904e18dabda712429eee654c64cb052b5a02e
maxxbot <brio0023@umn.edu>
2018-12-16 22:50:17 -0600

    Finished worktodo.py

--- Worktodo/worktodo.py
+++ Worktodo/worktodo.py
@@ -1,1 +1,63 @@
 #Prime95 worktodo.txt file generator
+
+import math
+import numpy
+
+prange = 100000
+    
+
+primes = []
+for possiblePrime in range(127, prange + 1):
+
+	isPrime = True
+	for num in range(2, int(possiblePrime ** 0.5) + 1):
+		if possiblePrime % num == 0:
+			isPrime = False
+			break
+	if isPrime:
+		primes.append(possiblePrime)
+    
+ 
+for i in range(len(primes)):
+	print(primes[i])
+
+with open('worktodo.txt', 'w') as wf:
+	wf.write('[Worker #1]\n\n')
+	for i in range(0 , len(primes), 4):
+		wf.write('AdvancedTest=' + str(primes[i]) + '\n')
+	wf.write('[Worker #2]\n\n')
+	for i in range(1 , len(primes), 4):
+		wf.write('AdvancedTest=' + str(primes[i]) + '\n')
+	wf.write('[Worker #3]\n\n')
+	for i in range(2 , len(primes), 4):
+		wf.write('AdvancedTest=' + str(primes[i]) + '\n')
+	wf.write('[Worker #4]\n\n')
+	for i in range(3 , len(primes), 4):
+		wf.write('AdvancedTest=' + str(primes[i]) + '\n')
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
